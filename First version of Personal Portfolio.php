0815d9cdcc7b0ea07904e1d1c678ee8ff993a0c6
jasontew-956 <tewej01@uwgb.edu>
2025-05-03 12:38:47 -0500

    Add files via upload

---
+++ First version of Personal Portfolio.php
@@ -0,0 +1,125 @@
+<!-- Copy the text below and save as html. --> 
+
+<!DOCTYPE html> 
+
+<html lang="en"> 
+
+    <head> 
+
+        <meta charset="utf-8"> 
+
+        <meta name="author" content ="Jason Tewes"> 
+
+        <meta name="description" content="This is an HTML template"> 
+
+        /* <link href="name of file" rel="stylesheet"> */ 
+
+        <title> 
+
+            Short Profile 
+
+        </title>  
+
+        <style> 
+
+            /* The CSS goes here */ 
+
+            table, tr, th, th { 
+
+                border: 2px, solid grey;
+
+            h2, h3 {color: darkgreen, blueviolet; 
+
+                font-family: 'Courier New', Courier, monospace; 
+
+            } 
+
+        </style> 
+
+    </head> 
+
+ 
+
+    <body> 
+
+        <!-- This is the info section. --> 
+
+        <section> 
+
+            <h2 style="color: darkgreen; font-family: 'Courier New', Courier, monospace;">IT Major with a minor in Business Administration</h2> 
+
+            <h3>HomeTown: Oconomowoc, Wisconsin</h3> 
+
+        <section> 
+
+ 
+
+        <section> 
+
+            <table> 
+
+                <caption>Spring 2025</caption> 
+
+                <tr> 
+
+                    <th>Department</th> 
+
+                    <th>Course</th> 
+
+                </tr> 
+
+                <tr> 
+
+                    <td>Computer science</td> 
+
+                    <td>Comp Sci 201</td> 
+
+                </tr> 
+
+                <tr> 
+
+                    <td>Math</td> 
+
+                    <td>Math 260</td> 
+
+                </tr> 
+
+            </table> 
+
+            <h2>Spring 2025<h2> 
+
+            <ul> 
+
+                <li>Comp Sci 201</li> 
+
+                <li>Math 260</li> 
+
+                <li>WF 200</li> 
+
+                <li>Econ 202</li> 
+
+                <li>ENV SCI 102</li> 
+
+        </section> 
+
+ 
+
+        <section> 
+
+        <h2>Work Experience 
+
+         <ol> 
+
+            <li><strong>Stocker: </strong>Piggly Wiggly</li> 
+
+            <ol> 
+
+        <section> 
+
+ 
+
+    </body> 
+
+ 
+
+</html> 
